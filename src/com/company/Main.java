03894d3bc72161e0e7db19ea008ae74d30d9b72f
nakamoto <hartmnn.p@gmail.com>
2018-06-04 11:47:52 -0300

    1st exercise, method 2: Adding contacts using a array 2D loop

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,26 +6,44 @@ public class Main {
 	// write your code here
         ContactMananger mananger = new ContactMananger();
 
-        Contact leticia = new Contact();
-        leticia.email = "leite.llp@gmail.com";
-        leticia.name = "Leticia";
-        leticia.phone = "961197806";
+        String[/*row*/][/*column*/] contacts = {
+                /* Names */
+                {"Leticia", "Ricardo", "Pedro"},
+                /* Emails */
+                {"leite.llp@gmail.com", "rsricarsan@gmail.com", "hartmnn.p@gmail.com"},
+                /* Phone */
+                {"961197806", "961607778", "968702002"}
+        };
 
-        Contact pedro = new Contact();
-        pedro.name = "Pedro";
-        pedro.email = "hartmnn.p@gmail.com";
-        pedro.phone = "961197806";
+         /**
+          * Each column in a row: i,j
+          * System.out.println(contacts[i][j] + " " + i + "," + j);
+          *
+          * Each row in a column
+          * System.out.println(contacts[j][i] + " " + j + "," + i);
+          *
+          * */
 
-        Contact pai = new Contact();
-        pai.name = "Ricardo";
-        pai.email = "rsricarsan@gmail.com";
-        pai.phone = "961607778";
+        /* Adding contacts using array 2D loop */
+        for (int i = 0; i < contacts.length; i++){
+            //System.out.print("row i: " + i + "\n");
+            System.out.println("---------");
+            for (int j = 0; j < contacts[i].length; j++){
+                /* Each row in a column */
+                Contact cont = new Contact();
+                cont.name = contacts[j][i];
+                System.out.println(j + "," + i + " " + contacts[j][i]);
+                j++;
 
-        /* Adding multiples contacts using array */
-        Contact[] contactArray = {leticia, pai, pedro};
-        for (int i = 0; i < contactArray.length; i++){
-            mananger.addContact(contactArray[i]);
-            System.out.println(contactArray[i].name + " added");
+                cont.email = contacts[j][i];
+                System.out.println(j + "," + i + " " + contacts[j][i]);
+                j++;
+
+                cont.phone = contacts[j][i];
+                System.out.println(j + "," + i + " " + contacts[j][i]);
+
+                mananger.addContact(cont);
+            }
         }
 
         /* Fetching by name */
